02bae2553f11da632b9f595d51d7d720f584cee4
anne <anneirungu07@gmail.com>
2024-01-11 21:34:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    let left = dodger.style.left;
+    left = parseInt(left);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
